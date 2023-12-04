4052bcbbc049b43052134d4931f888e8d8a7345b
gmharrington24 <88356060+gmharrington24@users.noreply.github.com>
2023-12-03 16:35:36 -0800

    alb

--- main.tf
+++ main.tf
@@ -47,6 +47,58 @@ subnet_id = module.blog_vpc.public_subnets[0]
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  target_groups = [
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+      targets = {
+  my target = {
+    target_id = aws_instance.blog
+    port = 80
+   }
+ }
+}
+  ]
+
+listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "h1"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+
+    tags = {
+    Environment = "Development"
+    }
+    }
+  }
+
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"

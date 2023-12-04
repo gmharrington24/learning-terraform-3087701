b563ab1c9b9b8e10f733d1b6da1fabe6cb0e9216
gmharrington24 <88356060+gmharrington24@users.noreply.github.com>
2023-12-03 17:15:17 -0800

    alb-11

--- main.tf
+++ main.tf
@@ -55,19 +55,19 @@ module "alb" {
   subnets = module.blog_vpc.public_subnets
   security_groups = module.blog_sg.security_group_id
 
-  target_groups = 
-    
+  target_groups = [
       name_prefix      = "blog"
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
       targets = {
-     my_target
-    target_id = aws_instance.blog
+     my_target {
+    target_id = "aws_instance.blog"
     port = 80
    }
  }
-
+ }
+]
 
 
 
